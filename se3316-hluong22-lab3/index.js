bb0ad54a818205957cd966be641e920b012fea62
MikeyLuo <hluong22@uwo.ca>
2020-10-29 13:50:58 -0400

    Removed Duplications

--- se3316-hluong22-lab3/index.js
+++ se3316-hluong22-lab3/index.js
@@ -1,6 +1,7 @@
 const express = require('express'); // Importing the express library
 const app = express();
 const port = 3000;
+const router = express.Router(); // Create the route object
 
 // Parts store
 const parts = [
@@ -13,28 +14,36 @@ const parts = [
 // Setup server front-end code
 app.use('/', express.static('static'));
 
+// Setup middleware to do logging
+app.use((req,res,next) => // for all routes
+{
+    console.log(`${req.method} request for ${req.url}`);
+    next(); // Keep going
+});
 
 // get list of parts
-app.get('/api/parts', (req, res) => 
+router.get('/', (req, res) => 
 {
     // logs the message in the format get request and url
-    console.log(`GET request for ${req.url}`);
+    //console.log(`GET request for ${req.url}`);
     res.send(parts);
 });
 
 // Get details for a given part
-app.get('/api/parts/:part_id', (req,res) =>
+router.get('/:part_id', (req,res) =>
 {
     const id = req.params.part_id; // String value
-    console.log(`GET request for ${req.url}`);
+    //console.log(`GET request for ${req.url}`);
     const part = parts.find(p => p.id === parseInt(id)); // Convert the string to an integer
     if (part){
-        res.send(part); // Send the part if id was found
+        res.send(part); // Send the part id if the id was found
     }
     else{
         res.status(404).send(`part ${id} was not found!`);
     }
 });
+
+app.use('/api/parts', router)
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
